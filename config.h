2a6c5e012b4ba97493d7f65f7a3062e58aca29b6
lucas-mior <lucas.mior@tutamail.com>
2021-11-23 01:12:09 -0300

    meus blocos

--- config.h
+++ config.h
@@ -1,29 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"sb-tasks",	10,	26},
-	/* {"",	"sb-music",	0,	11}, */
-	{"",	"sb-pacpackages",	0,	8},
-	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",				9000,	21}, */
-	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
-	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
-	{"",	"sb-volume",	0,	10},
-	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
-	{"",	"sb-internet",	5,	4},
-	/* {"",	"sb-iplocate", 0,	27}, */
-	{"",	"sb-help-icon",	0,	15},
+	/*Icon*/  /*Command*/    /*Update Interval*/    /*Update Signal*/
+	{"", "cat /tmp/ric 2>/dev/null",   0,    16},
+	{"", "0notmounted.sh",             0,    11},
+	{"", "0mounted.sh",                0,    12},
+	{"", "0phone.sh",                  0,    13},
+	{"", "musica.sh",                  0,     1},
+	{"", "vol.sh",                     0,     2},
+	{"", "mic.sh",                     0,     3},
+	{"", "bril.sh",                    0,     4},
+	{"", "news.sh",                    0,     8},
+	{"", "horario.sh",                 1,     0},
+	{"", "1memory.sh",                60,     0},
+	{"", "1cpu.sh",                   60,     0},
+	{"", "bateria.sh",                60,     5},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
