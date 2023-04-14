fa51e318d177d9d24b24866f975a45d5bb55124b
lucas-mior <lucas.mior@tutamail.com>
2023-04-14 18:09:04 -0300

    lucas: organize

--- config.h
+++ config.h
@@ -3,23 +3,25 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/    /*Update Interval*/    /*Update Signal*/
-	{"", "cat /tmp/ric 2>/dev/null",   0,    10},
-	{"", "clipout.sh",                 0,    11},
-	{"", "0ydl.sh",                    0,    12},
-	{"", "0notmounted.sh",             0,    13},
-	{"", "0mounted.sh",                0,    14},
-	{"", "0phone.sh",                  0,    15},
-	{"", "musica.sh",                  0,    16},
-	{"", "vol.sh",                     0,    17},
-	{"", "mic.sh",                     0,    18},
-	{"", "bril.sh",                    0,    19},
-	/* {"", "news.sh",                    0,    20}, */
-	{"", "rorario",                    1,    21},
-	{"", "1memory.sh",                60,    22},
-	{"", "1cpu.sh",                   60,    23},
-	{"", "internet.awk /sys/class/net/*/operstate", 60,    24},
-	/* {"", "1bluetooth.sh",             60,    25}, */
-	{"", "bateria.awk /sys/class/power_supply/BAT0/uevent", 60,    26},
+	{"", "cat /tmp/ric 2>/dev/null",        0,    10},
+	{"", "clipout.sh",                      0,    11},
+	{"", "0ydl.sh",                         0,    12},
+	{"", "0notmounted.sh",                  0,    13},
+	{"", "0mounted.sh",                     0,    14},
+	{"", "0phone.sh",                       0,    15},
+	{"", "musica.sh",                       0,    16},
+	{"", "vol.sh",                          0,    17},
+	{"", "mic.sh",                          0,    18},
+	{"", "bril.sh",                         0,    19},
+	{"", "rorario",                         1,    21},
+	{"", "1memory.sh",                     60,    22},
+	{"", "1temp.sh",                       60,    23},
+	{"", "1internet.awk"
+		 " /sys/class/net/w*/flags"
+	     " /sys/class/net/*/operstate",     0,    27},
+	{"", "1bluetooth.sh",                  80,    25},
+	{"", "bateria.awk "
+		 "/sys/class/power_*/BAT0/uevent", 60,    26},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
