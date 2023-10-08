e1916568ed3bf0e4a5fa1b3099d287c657772f34
lucas-mior <lucas.mior@tutamail.com>
2023-10-08 20:18:09 -0300

    refactor block_clock

--- main.c
+++ main.c
@@ -22,25 +22,42 @@ static void signal_handler(int);
 static void status_bar_update(bool);
 
 int main(void) {
-    int64 seconds = -1;
-    struct timespec sleep_time;
-    struct timespec to_sleep;
     int screen;
-    char *BLOCK_CLOCK;
 
-    struct sigaction signal_action;
-    struct sigaction signal_child_action;
-    signal_child_action.sa_handler = SIG_DFL;
-    signal_child_action.sa_flags = SA_NOCLDWAIT;
+    {
+        struct sigaction signal_action;
+        struct sigaction signal_child_action;
+        signal_child_action.sa_handler = SIG_DFL;
+        signal_child_action.sa_flags = SA_NOCLDWAIT;
 
-    sleep_time.tv_sec = 1;
-    sleep_time.tv_nsec = 0;
+        for (int i = SIGRTMIN; i <= SIGRTMAX; i += 1)
+            signal(i, SIG_IGN);
 
-    if ((BLOCK_CLOCK = getenv("BLOCK_CLOCK")) == NULL) {
-        fprintf(stderr, "BLOCK_CLOCK environmental variable is not defined\n.");
+        for (uint i = 0; i < LENGTH(blocks); i += 1) {
+            if (blocks[i].signal > 0) {
+                signal(SIGRTMIN + blocks[i].signal, signal_handler);
+                sigaddset(&signal_action.sa_mask, SIGRTMIN + blocks[i].signal);
+            }
+        }
+        signal(SIGRTMIN + clock_signal, signal_handler);
+        sigaddset(&signal_action.sa_mask, SIGRTMIN + clock_signal);
+
+        signal_action.sa_sigaction = button_handler;
+        signal_action.sa_flags = SA_SIGINFO;
+        sigaction(SIGUSR1, &signal_action, NULL);
+        sigaction(SIGCHLD, &signal_child_action, NULL);
     }
-    clock_signal = atoi(BLOCK_CLOCK);
-    clock_output[0] = (char) clock_signal;
+
+    {
+        char *BLOCK_CLOCK;
+        if ((BLOCK_CLOCK = getenv("BLOCK_CLOCK")) == NULL) {
+            fprintf(stderr,
+                    "BLOCK_CLOCK environmental variable is not defined\n.");
+            exit(EXIT_FAILURE);
+        }
+        clock_signal = atoi(BLOCK_CLOCK);
+        clock_output[0] = (char) clock_signal;
+    }
 
     if ((display = XOpenDisplay(NULL)) == NULL) {
         fprintf(stderr, "Error opening X display\n");
@@ -49,30 +66,21 @@ int main(void) {
     screen = DefaultScreen(display);
     root = RootWindow(display, screen);
 
-    for (int i = SIGRTMIN; i <= SIGRTMAX; i += 1)
-        signal(i, SIG_IGN);
+    {
+        struct timespec sleep_time;
+        struct timespec to_sleep;
+        int64 seconds = -1;
+        sleep_time.tv_sec = 1;
+        sleep_time.tv_nsec = 0;
 
-    for (uint i = 0; i < LENGTH(blocks); i += 1) {
-        if (blocks[i].signal > 0) {
-            signal(SIGRTMIN + blocks[i].signal, signal_handler);
-            sigaddset(&signal_action.sa_mask, SIGRTMIN + blocks[i].signal);
+        while (true) {
+            to_sleep = sleep_time;
+            get_block_outputs(seconds);
+            status_bar_update(false);
+
+            while (nanosleep(&to_sleep, &to_sleep) < 0);
+            seconds += sleep_time.tv_sec;
         }
-    }
-    signal(SIGRTMIN + clock_signal, signal_handler);
-    sigaddset(&signal_action.sa_mask, SIGRTMIN + clock_signal);
-
-    signal_action.sa_sigaction = button_handler;
-    signal_action.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &signal_action, NULL);
-    sigaction(SIGCHLD, &signal_child_action, NULL);
-
-    while (true) {
-        to_sleep = sleep_time;
-        get_block_outputs(seconds);
-        status_bar_update(false);
-
-        while (nanosleep(&to_sleep, &to_sleep) < 0);
-        seconds += sleep_time.tv_sec;
     }
 }
 
@@ -254,19 +262,12 @@ void block_clock(int button) {
     time_t seconds_since_epoch;
     struct tm t;
     char *week;
+    char *week_names[] = { "dom", "seg", "ter", "qua", "qui", "sex", "sáb" };
     char *output = clock_output + 1;
 
     seconds_since_epoch = time(NULL);
     t = *localtime(&seconds_since_epoch);
-    week = ((char *[]) {
-        "dom",
-        "seg",
-        "ter",
-        "qua",
-        "qui",
-        "sex",
-        "sáb"
-    })[t.tm_wday];
+    week = week_names[t.tm_wday];
 
     snprintf(output, BLOCK_OUTPUT_LENGTH - 1,
             "📅 %s %02d/%02d %02d:%02d:%02d\n",
