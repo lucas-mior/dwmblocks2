e45279b9e4cb40f2ab4a4d55ab20594e782921b6
lucas-mior <lucas.mior@tutamail.com>
2023-10-08 15:33:17 -0300

    use lesser signal numbers

--- blocks.h
+++ blocks.h
@@ -3,22 +3,22 @@
 
 static Block blocks[] = {
 /* command                  update interval   update signal */
-{"block_recording.sh",                    0,  10},
-{"block_clipboard.sh",                    0,  11},
-{"block_ytdlp.sh",                        0,  12},
-{"block_drives_not.sh",                   0,  13},
-{"block_drives_mount.sh",                 0,  14},
-{"block_phone.sh",                        0,  15},
-{"block_music.sh",                        0,  16},
-{"block_volume.sh",                       0,  17},
-{"block_microphone.sh",                   0,  18},
-{"block_brilho.sh",                       0,  19},
+{"block_recording.sh",                    0,  1},
+{"block_clipboard.sh",                    0,  2},
+{"block_ytdlp.sh",                        0,  3},
+{"block_drives_not.sh",                   0,  4},
+{"block_drives_mount.sh",                 0,  5},
+{"block_phone.sh",                        0,  6},
+{"block_music.sh",                        0,  7},
+{"block_volume.sh",                       0,  8},
+{"block_microphone.sh",                   0,  9},
+{"block_brilho.sh",                       0, 10},
 //{"block_news.sh", 0, 20},
-{"block_memory.sh",                      60,  22},
-{"block_temperature.sh",                 60,  23},
-{"block_internet.sh",                     0,  27},
-{"block_bluetooth.sh",                   80,  25},
-{"block_bateria.sh",                     60,  26},
+{"block_memory.sh",                      60, 11},
+{"block_temperature.sh",                 60, 12},
+{"block_internet.sh",                     0, 13},
+{"block_bluetooth.sh",                   80, 14},
+{"block_bateria.sh",                     60, 15},
 };
 
 #endif
