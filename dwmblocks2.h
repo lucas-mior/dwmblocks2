771a3e42590ff1628d75cca0977602e38cd455f3
lucas-mior <lucas.mior@tutamail.com>
2023-10-13 12:30:46 -0300

    pipe does not need to be atomic

--- dwmblocks2.h
+++ dwmblocks2.h
@@ -45,7 +45,7 @@ typedef struct Block {
     char *signal_var_name;
     int interval;
     int signal;
-    _Atomic int pipe;
+    int pipe;
     char output[BLOCK_OUTPUT_LENGTH];
     uint32 length;
 } Block;
