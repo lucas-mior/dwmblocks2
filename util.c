787284c414fb635e851501f0cb41eeb57987a812
lucas-mior <lucas.mior@tutamail.com>
2025-11-06 14:46:14 -0300

    update util.c from brn2

--- util.c
+++ util.c
@@ -212,19 +212,13 @@ _Generic((S), \
 // clang-format on
 
 static char *notifiers[2] = {"dunstify", "notify-send"};
+static int64 util_page_size = 0;
 
-static char *xstrdup(char *);
-static int32 snprintf2(char *, int, char *, ...);
 static void error(char *, ...);
 static void fatal(int) __attribute__((noreturn));
-static int32 util_string_int32(int32 *, const char *);
-static int util_command(const int, char **);
-static void util_die_notify(char *, const char *, ...)
-    __attribute__((noreturn));
 static void util_segv_handler(int32) __attribute__((noreturn));
 static char *itoa2(long, char *);
 static long atoi2(char *);
-static int64 util_page_size = 0;
 INLINE void *memchr64(void *pointer, int32 value, int64 size);
 
 #if !defined(CAT)
@@ -311,6 +305,14 @@ memchr64(void *pointer, int32 value, int64 size) {
 INLINE int64
 strlen64(char *string) {
     size_t len = strlen(string);
+    return (int64)len;
+}
+
+INLINE int64
+strnlen64(char *string, int64 size) {
+    size_t len;
+    assert((uint64)size <= SIZE_MAX);
+    len = strnlen(string, (size_t)size);
     return (int64)len;
 }
 
@@ -518,7 +520,7 @@ xcalloc(const size_t nmemb, const size_t size) {
     return p;
 }
 
-char *
+static char *
 xstrdup(char *string) {
     char *p;
     int64 length;
@@ -574,7 +576,7 @@ xpthread_mutex_destroy(pthread_mutex_t *mutex) {
     return;
 }
 
-int32
+static int32
 snprintf2(char *buffer, int size, char *format, ...) {
     int n;
     va_list args;
@@ -595,7 +597,7 @@ snprintf2(char *buffer, int size, char *format, ...) {
 }
 
 #if OS_WINDOWS
-int
+static int
 util_command(const int argc, char **argv) {
     char cmdline[1024] = {0};
     int64 j = 0;
@@ -679,7 +681,7 @@ util_command(const int argc, char **argv) {
     return (int)exit_code;
 }
 #else
-int
+static int
 util_command(const int argc, char **argv) {
     pid_t child;
     int status;
@@ -778,7 +780,7 @@ util_segv_handler(int32 unused) {
     _exit(EXIT_FAILURE);
 }
 
-int32
+static int32
 util_string_int32(int32 *number, const char *string) {
     char *endptr;
     long x;
@@ -794,7 +796,7 @@ util_string_int32(int32 *number, const char *string) {
     }
 }
 
-void
+static void __attribute__((noreturn))
 util_die_notify(char *program_name, const char *format, ...) {
     int32 n;
     va_list args;
