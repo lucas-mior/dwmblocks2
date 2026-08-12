64af7b66e1a49246c257e931cc3d08833d6206c2
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 16:03:34 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -45,6 +45,7 @@ if [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Wno-pre-c11-compat"
     CFLAGS="$CFLAGS -Wno-unsafe-buffer-usage"
     CFLAGS="$CFLAGS -Wno-unused-macros"
+    CFLAGS="$CFLAGS -Wno-used-but-marked-unused"
 fi
 
 LDFLAGS="$LDFLAGS -lm $(pkg-config x11 --libs)"
