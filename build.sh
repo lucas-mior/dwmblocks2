5f92c8923d2dfbffb27c0b127e491d4fabc84ece
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 16:33:33 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 # shellcheck disable=SC2086
 
 dir=$(dirname "$(readlink -f "$0")")
-# shellcheck source=/dev/null
-. "$dir/cbase/common.sh"
-
 cd "$dir" || exit
+
+# shellcheck source=./cbase/common.sh
+. "./cbase/common.sh"
+
 program=$(common_get_program "$0")
 script=$(basename "$0")
 common_build_parse_args "$@"
