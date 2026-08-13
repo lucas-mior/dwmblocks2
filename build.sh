8348751421ca70240eef8bf01f21105e34b2a6c6
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 16:34:02 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-missing-field-initializers"
 
-if [ "$CC" = "clang" ]; then
+if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-assign-enum"
     CFLAGS="$CFLAGS -Wno-c++-keyword"
